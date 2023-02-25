c970217ec17a23a1351c8574dbac32f47d43db0a
Tanuv Nair <tanuvnair@gmail.com>
2023-02-25 23:23:49 +0530

    Updated code to display Supply and Demand rows and columns

--- LPP.java
+++ LPP.java
@@ -17,22 +17,53 @@ public class LPP {
         int numSource = in.nextInt();
         int numDestination = in.nextInt();
 
-        int[][] question = new int[numSource + 1][numDestination + 1];
+        numSource++;
+        numDestination++;
 
-        for(int i = 0; i < numSource; i++) {
-            for(int j = 0; j < numDestination; j++) {
+        int[][] question = new int[numSource][numDestination];
+
+        for(int i = 0; i < numSource - 1; i++) {
+            for(int j = 0; j < numDestination - 1; j++) {
                 System.out.print("Enter x[" + (i + 1) + "][" + (j + 1) + "]: ");
                 question[i][j] = in.nextInt();
             }
         }
 
+        for(int i = 0; i < numSource - 1; i++) {
+            System.out.println("Enter Supply from S" + (i + 1) +": ");
+            question[i][numDestination - 1] = in.nextInt();
+        }
+        
+        for(int i = 0; i < numDestination - 1; i++) {
+            System.out.println("Enter Demand from D" + (i + 1) +": ");
+            question[numSource - 1][i] = in.nextInt();
+        }
+
+
         System.out.println("Question:");
+        for(int i = 0; i < numDestination + 1; i++) {
+            if(i == 0) {
+                System.out.print("\t");
+                continue;
+            }
+            if(i == numDestination) {
+                System.out.print("Supply" + "\t");
+            } 
+            else {
+                System.out.print("D" + i + "\t");
+            }
+        }
+        System.out.println();
         for(int i = 0; i < numSource; i++) {
             for(int j = 0; j < numDestination; j++) {
-                if(j == 0) {
-                    System.out.print("S ");
+                if(i == numSource - 1 && j == 0) {
+                    System.out.print("Demand" + "\t");
+                    continue;
                 }
-                System.out.print(question[i][j] + " ");
+                if(j == 0) {
+                    System.out.print("S" + (i + 1) + "\t");
+                }
+                System.out.print(question[i][j] + "\t");
             }
             System.out.println();
         }
